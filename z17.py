e081be0c0374e675ebb5baf73558fbce93f3a61a
S. Son <seungson@live.com>
2024-08-07 08:07:31 -0700

    c001

--- z17.py
+++ z17.py
@@ -4,7 +4,7 @@ y,s,h=bl=[15,16,17]
 n,a=16,random.randint(7,15)*16+random.randint(2,14)
 while (bl.count(h)%2)*(h%16)*(h&240):
     if e:=pygame.event.get(768):
-        s=(e[0].key%4*17+15)%49-n
+        s=(e[0].key%4*17+15)%49 -n
     bl=bl[a!=h:]+[h+s]
     sf('black')
     if a==h:
