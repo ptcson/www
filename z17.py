23a5b8d2a0f768d7dd34ff54624633747dd3c089
S. Son <seungson@live.com>
2024-08-07 07:58:49 -0700

    17 lines

--- z17.py
+++ z17.py
@@ -1,8 +1,8 @@
 import pygame,random
 sf=pygame.display.set_mode([15**2]*2).fill
 y,s,h=bl=[15,16,17]
-n,a=16,random.randint(7,15)*16+random.randint(5,15)
-while (bl.count(h)<2)*(h%16)*(h&240):
+n,a=16,random.randint(7,15)*16+random.randint(2,14)
+while (bl.count(h)%2)*(h%16)*(h&240):
     if e:=pygame.event.get(768):
         s=(e[0].key%4*17+15)%49-n
     bl=bl[a!=h:]+[h+s]
